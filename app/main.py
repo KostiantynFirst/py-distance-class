90d7ce49c727c9f9167393e2124ef026fd147c1e
Kostiantyn <goncharov.kostiantyn@gmail.com>
2026-02-17 13:42:21 +0200

    removed Unnecessary else blocks else

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from functools import total_ordering
 
 
@@ -13,41 +14,39 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: object) -> "Distance":
+    def __add__(self, other: object) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
 
-    def __iadd__(self, other: object) -> "Distance":
+    def __iadd__(self, other: object) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, (int, float)):
+            return self
+        if isinstance(other, (int, float)):
             self.km += other
-        else:
-            return NotImplemented
+            return self
+        return NotImplemented
 
-        return self
-
-    def __mul__(self, other: int | float) -> "Distance":
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         return NotImplemented
 
-    def __rmul__(self, other: int | float) -> "Distance":
+    def __rmul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return self * other
         return NotImplemented
 
-    def __truediv__(self, other: int | float) -> "Distance":
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             if other == 0:
                 raise ZeroDivisionError("division by zero")
             quotient = self.km / other
             return Distance(round(quotient, 2))
-        else:
-            return NotImplemented
+        return NotImplemented
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
